c97692b9771a4d82b50f7bb88891b267eff6cb9e
diverglovsky <divandaarya@gmail.com>
2019-10-26 18:58:12 +0700

    update

--- fl.py
+++ fl.py
@@ -1,13 +1,11 @@
 import requests
 import time
 from requests.exceptions import Timeout
-
-a = list(range(210900010,211000000))
-    
+a = list(range(210000001,210150000))
 i=1
 for tokens in a:
 	try:
-		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 29b0534f029ff4614cffbd466a5c1b2327f65f72'}
+		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 483c27a285331951b82d14f7a0d1eaf36adb99ac'}
 		response = requests.post('https://id-api.spooncast.net/users/'+str(tokens)+'/follow/',headers=headers,timeout=1)
 		response2 = requests.post('http://id-api.spooncast.net/users/'+str(tokens)+'/fanmessages/',headers=headers,json={'contents':'Fan back dong kk 😚'},timeout=1)
 		print(response)
@@ -19,4 +17,3 @@ for tokens in a:
 		print('ke '+str(tokens)+' gagal')
 	except:
 		print('error internet')
-	#time.sleep(5)

877c8163e3eef178d254dd1c71590d91574b0a3a
diverglovsky <divandaarya@gmail.com>
2019-10-26 19:22:10 +0700

    Update fl8.py

--- fl8.py
+++ fl8.py
@@ -7,7 +7,7 @@ for tokens in a:
 	try:
 		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 483c27a285331951b82d14f7a0d1eaf36adb99ac'}
 		response = requests.post('https://id-api.spooncast.net/users/'+str(tokens)+'/follow/',headers=headers,timeout=1)
-		response2 = requests.post('http://id-api.spooncast.net/users/'+str(tokens)+'/fanmessages/',headers=headers,json={'contents':'Fan back dong kk 😚'},timeout=1)
+		response2 = requests.post('http://id-api.spooncast.net/users/'+str(tokens)+'/fanmessages/',headers=headers,json={'contents':'Fan back dong kk'},timeout=1)
 		print(response)
 		print(response2)
 		print(i)

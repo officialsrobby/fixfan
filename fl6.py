a031224c05a94cce8ab6419bce6b6f669ee16dd6
diverglovsky <divandaarya@gmail.com>
2019-10-26 19:02:41 +0700

    crea

---
+++ fl6.py
@@ -0,0 +1,19 @@
+import requests
+import time
+from requests.exceptions import Timeout
+a = list(range(210750001,210900000))
+i=1
+for tokens in a:
+	try:
+		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 483c27a285331951b82d14f7a0d1eaf36adb99ac'}
+		response = requests.post('https://id-api.spooncast.net/users/'+str(tokens)+'/follow/',headers=headers,timeout=1)
+		response2 = requests.post('http://id-api.spooncast.net/users/'+str(tokens)+'/fanmessages/',headers=headers,json={'contents':'Fan back dong kk 😚'},timeout=1)
+		print(response)
+		print(response2)
+		print(i)
+		i+=1
+		print(tokens)
+	except Timeout:
+		print('ke '+str(tokens)+' gagal')
+	except:
+		print('error internet')

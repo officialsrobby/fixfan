4ab12d52b328da6dbaaa670e1b473921420cde07
diverglovsky <divandaarya@gmail.com>
2019-10-28 15:58:45 +0700

    Update fl4.py

--- fl4.py
+++ fl4.py
@@ -5,7 +5,7 @@ a = list(range(210450001,210600000))
 i=1
 for tokens in a:
 	try:
-		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 483c27a285331951b82d14f7a0d1eaf36adb99ac'}
+		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token acc3a4d5425d3b7aa5f605cb972796d5e9985a21'}
 		response = requests.post('https://id-api.spooncast.net/users/'+str(tokens)+'/follow/',headers=headers,timeout=1)
 		response2 = requests.post('http://id-api.spooncast.net/users/'+str(tokens)+'/fanmessages/',headers=headers,json={'contents':'Fan back dong kk'},timeout=1)
 		print(response)
